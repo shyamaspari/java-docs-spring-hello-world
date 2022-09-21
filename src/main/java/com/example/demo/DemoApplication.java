7b3dd53938ba929dd57f62e27b2ee660f5f81727
shyam pasupula <pkumar@ptc.com>
2022-09-21 23:17:49 +0530

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,6 +16,6 @@ public class DemoApplication extends SpringBootServletInitializer {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World! shyam";
+		return "<h2>Hello World! shyam</h2>";
 	}
 }
